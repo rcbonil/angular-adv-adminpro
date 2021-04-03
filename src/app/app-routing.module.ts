0794443268d9f95a982542a54825667fba9b01c1
Rolland Castillo <rolland.castillo@gmail.com>
2021-04-03 01:07:56 -0400

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistreComponent } from './auth/registre/registre.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphica1Component } from './pages/graphica1/graphica1.component';
+import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
+import { PagesComponent } from './pages/pages.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graphica1', component: Graphica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistreComponent },
+  { path: '**', component: NopagesfoundComponent }
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routers
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistreComponent } from './auth/registre/registre.component';
+import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shares/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shares/sidebar/sidebar.component';
+import { HeaderComponent } from './shares/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphica1Component } from './pages/graphica1/graphica1.component';
+import { PagesComponent } from './pages/pages.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistreComponent,
+    NopagesfoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProgressComponent,
+    Graphica1Component,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
