94f3f2642df21b237aaeb8263e7e802bab263c2d
Rolland Castillo <rolland.castillo@gmail.com>
2021-04-03 13:13:34 -0400

    Agregar de nuevo el folde de pages

---
+++ src/app/pages/graphica1/graphica1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphica1',
+  templateUrl: './graphica1.component.html',
+  styles: [
+  ]
+})
+export class Graphica1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,31 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graphica1Component } from './graphica1/graphica1.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graphica1', component: Graphica1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
+];
+
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class PagesRoutingModule { }
